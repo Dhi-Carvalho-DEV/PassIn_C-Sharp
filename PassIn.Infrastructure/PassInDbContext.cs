41b186cedc1cc100ee79e91de4f16573bce4068b
Dhi-Carvalho-DEV <dhi.carvalho.dev@gmail.com>
2024-04-17 18:17:29 -0300

    Adicionar arquivos de projeto.

---
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Infrastructure
+{
+    public class PassInDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<CheckIn> CheckIns { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=D:\\Rocketseat\\nlw_unite\\C#\\PassIn\\PassInDb.db");
+        }
+    }
+}
